c81a14751a9594102fe19f0712a7920c5e22cf88
kiki-kanri <a470666@gmail.com>
2024-03-23 05:30:03 +0800

    feat(node): add multi-platform support (linux/amd64, linux/arm64)

--- nodejs/build.sh
+++ nodejs/build.sh
@@ -5,18 +5,28 @@ declare -A versions=(
 	["21"]="21"
 )
 
+platforms="linux/amd64,linux/arm64"
+
 for NODE_VERSION in "${!versions[@]}"; do
 	# Base alpine
 	node_image=node:${versions[$NODE_VERSION]}-alpine
 	tag="kikikanri/node$NODE_VERSION:base-alpine"
 	docker pull $node_image &&
-		docker build -f ./base-alpine -t $tag --build-arg NODE_IMAGE=$node_image . &&
-		docker push $tag || exit 1
+		docker buildx build \
+			-f ./base-alpine \
+			-t $tag \
+			--build-arg NODE_IMAGE=$node_image \
+			--platform $platforms \
+			--push . || exit 1
 
 	# Base alpine puppeteer
 	base_image="kikikanri/node$NODE_VERSION:base-alpine"
 	tag="kikikanri/node$NODE_VERSION:puppeteer"
 	docker pull $base_image &&
-		docker build -f ./puppeteer -t $tag --build-arg BASE_IMAGE=$base_image . &&
-		docker push $tag || exit 1
+		docker buildx build \
+			-f ./puppeteer \
+			-t $tag \
+			--build-arg BASE_IMAGE=$base_image \
+			--platform $platforms \
+			--push . || exit 1
 done
