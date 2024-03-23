9a738be32c3fbec0c0cc9217c8351821769354e1
kiki-kanri <a470666@gmail.com>
2024-03-23 12:42:26 +0800

    feat(python): add multi-platform support (linux/amd64, linux/arm64)

--- python/build.sh
+++ python/build.sh
@@ -7,23 +7,20 @@ declare -A versions=(
 
 for PY_VERSION in "${!versions[@]}"; do
 	FULL_PY_VERSION=${versions[$PY_VERSION]}
-	BUILD_ARGS="--build-arg FULL_PY_VERSION=$FULL_PY_VERSION --build-arg PY_VERSION=$PY_VERSION"
+	BUILD_ARGS="--build-arg FULL_PY_VERSION=$FULL_PY_VERSION --build-arg PY_VERSION=$PY_VERSION --platform linux/amd64,linux/arm64 --push"
 
 	# Base alpine
 	tag="kikikanri/python$PY_VERSION:base-alpine"
 	docker pull python:$FULL_PY_VERSION-alpine &&
-		docker build -f ./base-alpine -t $tag $BUILD_ARGS . &&
-		docker push $tag || exit 1
+		docker buildx build -f ./base-alpine -t $tag $BUILD_ARGS . || exit 1
 
 	# Base alpine gcc
 	tag="kikikanri/python$PY_VERSION:base-alpine-gcc"
 	docker pull kikikanri/python$PY_VERSION:base-alpine &&
-		docker build -f ./base-alpine-gcc -t $tag $BUILD_ARGS . &&
-		docker push $tag || exit 1
+		docker buildx build -f ./base-alpine-gcc -t $tag $BUILD_ARGS . || exit 1
 
 	# Base slim
 	tag="kikikanri/python$PY_VERSION:base-slim"
 	docker pull python:$FULL_PY_VERSION-slim &&
-		docker build -f ./base-slim -t $tag $BUILD_ARGS . &&
-		docker push $tag || exit 1
+		docker buildx build -f ./base-slim -t $tag $BUILD_ARGS . || exit 1
 done
