5bb43b97bcbb6038fa63f5d02f1c76630855bde8
kiki-kanri <a470666@gmail.com>
2024-07-10 21:06:50 +0800

    feat: add telegram-bot-api files

---
+++ telegram-bot-api/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+name="telegram-bot-api"
+platforms="linux/amd64,linux/arm64"
+user="kikikanri"
+version="7.7"
+docker buildx build \
+	-t $user/$name:$version \
+	-t $user/$name:latest \
+	--build-arg VERSION=$version \
+	--builder multi-platform \
+	--platform $platforms \
+	--push .
