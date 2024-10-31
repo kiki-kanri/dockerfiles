00c803a9ff2fdf0995f6a275cd57d3ca3325ebb8
kiki-kanri <a470666@gmail.com>
2024-10-31 23:46:15 +0800

    feat(telegram-bot-api): add more platforms

--- telegram-bot-api/build.sh
+++ telegram-bot-api/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 name="telegram-bot-api"
-platforms="linux/amd64,linux/arm64"
+platforms="linux/386,linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le"
 user="kikikanri"
 version="7.11"
 docker buildx build \
