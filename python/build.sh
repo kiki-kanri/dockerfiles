fa5ba9cf8a90e65a9f6b4ff1b5967ba34e4d3359
kiki-kanri <a470666@gmail.com>
2024-11-21 18:27:55 +0800

    style: change scripts indentation to 4 spaces

--- python/build.sh
+++ python/build.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 declare -A versions=(
-	["3.11"]="3.11.9"
-	["3.12"]="3.12.7"
-	["3.13"]="3.13.0"
+    ["3.11"]="3.11.9"
+    ["3.12"]="3.12.7"
+    ["3.13"]="3.13.0"
 )
 
 for PY_VERSION in "${!versions[@]}"; do
-	FULL_PY_VERSION=${versions[$PY_VERSION]}
-	BUILD_ARGS="--build-arg FULL_PY_VERSION=$FULL_PY_VERSION --build-arg PY_VERSION=$PY_VERSION --builder multi-platform --platform linux/amd64,linux/arm64 --push"
+    FULL_PY_VERSION=${versions[$PY_VERSION]}
+    BUILD_ARGS="--build-arg FULL_PY_VERSION=$FULL_PY_VERSION --build-arg PY_VERSION=$PY_VERSION --builder multi-platform --platform linux/amd64,linux/arm64 --push"
 
-	# Base alpine
-	tag="kikikanri/python$PY_VERSION:base-alpine"
-	docker pull python:$FULL_PY_VERSION-alpine &&
-		docker buildx build -f ./base-alpine -t $tag $BUILD_ARGS . || exit 1
+    # Base alpine
+    tag="kikikanri/python$PY_VERSION:base-alpine"
+    docker pull python:$FULL_PY_VERSION-alpine &&
+        docker buildx build -f ./base-alpine -t $tag $BUILD_ARGS . || exit 1
 
-	# Base alpine gcc
-	tag="kikikanri/python$PY_VERSION:base-alpine-gcc"
-	docker pull kikikanri/python$PY_VERSION:base-alpine &&
-		docker buildx build -f ./base-alpine-gcc -t $tag $BUILD_ARGS . || exit 1
+    # Base alpine gcc
+    tag="kikikanri/python$PY_VERSION:base-alpine-gcc"
+    docker pull kikikanri/python$PY_VERSION:base-alpine &&
+        docker buildx build -f ./base-alpine-gcc -t $tag $BUILD_ARGS . || exit 1
 
-	# Base slim
-	tag="kikikanri/python$PY_VERSION:base-slim"
-	docker pull python:$FULL_PY_VERSION-slim &&
-		docker buildx build -f ./base-slim -t $tag $BUILD_ARGS . || exit 1
+    # Base slim
+    tag="kikikanri/python$PY_VERSION:base-slim"
+    docker pull python:$FULL_PY_VERSION-slim &&
+        docker buildx build -f ./base-slim -t $tag $BUILD_ARGS . || exit 1
 done
--- telegram-bot-api/build.sh
+++ telegram-bot-api/build.sh
@@ -5,9 +5,9 @@ platforms="linux/386,linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le"
 user="kikikanri"
 version="7.11"
 docker buildx build \
-	-t $user/$name:$version \
-	-t $user/$name:latest \
-	--build-arg VERSION=$version \
-	--builder multi-platform \
-	--platform $platforms \
-	--push .
+    -t $user/$name:$version \
+    -t $user/$name:latest \
+    --build-arg VERSION=$version \
+    --builder multi-platform \
+    --platform $platforms \
+    --push .
