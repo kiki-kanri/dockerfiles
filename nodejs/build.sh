5de9a66be7ff0c85820d9dbb2372f6ee3a1fee7a
kiki-kanri <a470666@gmail.com>
2025-01-23 09:44:36 +0800

    feat(node): add base-slim image

--- nodejs/build.sh
+++ nodejs/build.sh
@@ -31,4 +31,16 @@ for NODE_VERSION in "${!versions[@]}"; do
         --builder multi-platform \
         --platform $platforms \
         --push .
+
+    # Base slim
+    node_image=node:${versions[$NODE_VERSION]}-slim
+    tag="kikikanri/node$NODE_VERSION:base-slim"
+    docker pull $node_image
+    docker buildx build \
+        -f ./base-slim \
+        -t $tag \
+        --build-arg NODE_IMAGE=$node_image \
+        --builder multi-platform \
+        --platform $platforms \
+        --push .
 done
