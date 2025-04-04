18155de9f9abf73849d40fb95dc38d3f3e7aa727
kiki-kanri <a470666@gmail.com>
2025-04-04 10:59:37 +0800

    chore!: remove `telegram-bot-api` image

--- telegram-bot-api/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-args=""
-[ -n "$API_HASH" ] && args="$args --api-hash $API_HASH"
-[ -n "$API_ID" ] && args="$args --api-id $API_ID"
-[ -n "$LOCAL" ] && args="$args --local"
-[ -n "$HTTP_PORT" ] && args="$args -p $HTTP_PORT"
-telegram-bot-api $args
