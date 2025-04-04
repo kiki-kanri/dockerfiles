9783e31f654ac5ef10a6cd7312427ead0c4e598b
kiki-kanri <a470666@gmail.com>
2025-04-04 10:53:48 +0800

    refactor!: update python image
    - simplify build script with double for loop
    - add additional label information to image
    - rename image tag to a more common format

--- python/build.sh
+++ python/build.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
 set -e
-declare -A versions=(
+cd "$(dirname "$(readlink -f "$0")")"
+
+platforms='linux/amd64,linux/arm64'
+declare -A python_versions=(
     ["3.11"]="3.11.11"
     ["3.12"]="3.12.9"
     ["3.13"]="3.13.2"
 )
 
-for PY_VERSION in "${!versions[@]}"; do
-    FULL_PY_VERSION=${versions[$PY_VERSION]}
-    BUILD_ARGS="--build-arg FULL_PY_VERSION=$FULL_PY_VERSION --build-arg PY_VERSION=$PY_VERSION --builder multi-platform --platform linux/amd64,linux/arm64 --push"
+types=(
+    'alpine'
+    'alpine-gcc'
+    'slim'
+    'slim-gcc'
+)
 
-    # Base alpine
-    tag="kikikanri/python$PY_VERSION:base-alpine"
-    docker pull python:$FULL_PY_VERSION-alpine
-    docker buildx build -f ./base-alpine -t $tag $BUILD_ARGS .
-
-    # Base alpine gcc
-    tag="kikikanri/python$PY_VERSION:base-alpine-gcc"
-    docker pull kikikanri/python$PY_VERSION:base-alpine
-    docker buildx build -f ./base-alpine-gcc -t $tag $BUILD_ARGS .
-
-    # Base slim
-    tag="kikikanri/python$PY_VERSION:base-slim"
-    docker pull python:$FULL_PY_VERSION-slim
-    docker buildx build -f ./base-slim -t $tag $BUILD_ARGS .
+for major_python_version in "${!python_versions[@]}"; do
+    full_python_version="${python_versions[$major_python_version]}"
+    docker pull "python:$full_python_version-alpine" &
+    docker pull "python:$full_python_version-slim" &
+    wait
+    for type in "${types[@]}"; do
+        image_tag="kikikanri/python:$major_python_version-$type"
+        docker buildx build \
+            -f ./"$type" \
+            -t "$image_tag" \
+            --build-arg FULL_PYTHON_VERSION="$full_python_version" \
+            --build-arg IMAGE_TYPE="$type" \
+            --build-arg MAJOR_PYTHON_VERSION="$major_python_version" \
+            --builder multi-platform \
+            --no-cache \
+            --platform "$platforms" \
+            --push .
+    done
 done
