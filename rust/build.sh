13778b6137a720834bbccfbba8793211ef51c336
kiki-kanri <a470666@gmail.com>
2025-05-17 16:25:33 +0800

    feat: add `kikikanri/rust:slim-bookworm` image

---
+++ rust/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+cd "$SCRIPT_DIR"
+
+docker pull rust:slim-bookworm
+docker buildx build \
+    -f ./slim-bookworm \
+    -t 'kikikanri/rust:slim-bookworm' \
+    --no-cache \
+    --push .
