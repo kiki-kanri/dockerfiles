63eb600b30af5e3272283f249f4f5fed347d8047
kiki-kanri <a470666@gmail.com>
2025-05-29 10:23:45 +0800

    chore: wrap all variable expansions in some files with `${}`

--- node/build.sh
+++ node/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-cd "$SCRIPT_DIR"
+cd "${SCRIPT_DIR}"
 
 node_versions=(
     '22'
@@ -18,16 +18,16 @@ types=(
 
 for node_version in "${node_versions[@]}"; do
     for type in "${types[@]}"; do
-        image_tag="kikikanri/node:$node_version-$type"
-        node_tag="$node_version-$type"
-        docker pull "node:$node_tag"
+        image_tag="kikikanri/node:${node_version}-${type}"
+        node_tag="${node_version}-${type}"
+        docker pull "node:${node_tag}"
         docker buildx build \
-            -f ./"$type" \
-            -t "$image_tag" \
-            --build-arg NODE_TAG="$node_tag" \
+            -f ./"${type}" \
+            -t "${image_tag}" \
+            --build-arg NODE_TAG="${node_tag}" \
             --builder multi-platform \
             --no-cache \
-            --platform "$platforms" \
+            --platform "${platforms}" \
             --push .
     done
 done
--- python/build.sh
+++ python/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-cd "$SCRIPT_DIR"
+cd "${SCRIPT_DIR}"
 
 platforms='linux/amd64,linux/arm64'
 declare -A python_versions=(
@@ -20,21 +20,21 @@ types=(
 )
 
 for major_python_version in "${!python_versions[@]}"; do
-    full_python_version="${python_versions[$major_python_version]}"
-    docker pull "python:$full_python_version-alpine" &
-    docker pull "python:$full_python_version-slim" &
+    full_python_version="${python_versions[${major_python_version}]}"
+    docker pull "python:${full_python_version}-alpine" &
+    docker pull "python:${full_python_version}-slim" &
     wait
     for type in "${types[@]}"; do
-        image_tag="kikikanri/python:$major_python_version-$type"
+        image_tag="kikikanri/python:${major_python_version}-${type}"
         docker buildx build \
-            -f ./"$type" \
-            -t "$image_tag" \
-            --build-arg FULL_PYTHON_VERSION="$full_python_version" \
-            --build-arg IMAGE_TYPE="$type" \
-            --build-arg MAJOR_PYTHON_VERSION="$major_python_version" \
+            -f ./"${type}" \
+            -t "${image_tag}" \
+            --build-arg FULL_PYTHON_VERSION="${full_python_version}" \
+            --build-arg IMAGE_TYPE="${type}" \
+            --build-arg MAJOR_PYTHON_VERSION="${major_python_version}" \
             --builder multi-platform \
             --no-cache \
-            --platform "$platforms" \
+            --platform "${platforms}" \
             --push .
     done
 done
